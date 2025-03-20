7a404ae113b1aa1947d231b882250749aa3f0bbb
Sai Konkimalla <sai.konkimalla@gmail.com>
2025-03-20 00:54:19 -0700

    get list of all clusters associated with a particular file

---
+++ fread-fwrite/bootloader-sw-upload/objs/boot-sw-upload.d
@@ -0,0 +1,17 @@
+objs/boot-sw-upload.o: boot-sw-upload.c \
+ /home/saiko/Documents/stanford/winter_24_25/cs140e/priv-cs140e/libpi/include/rpi.h \
+ /home/saiko/Documents/stanford/winter_24_25/cs140e/priv-cs140e/libpi/libc/demand.h \
+ /home/saiko/Documents/stanford/winter_24_25/cs140e/priv-cs140e/libpi/include/gpio.h \
+ /home/saiko/Documents/stanford/winter_24_25/cs140e/priv-cs140e/libpi/include/rpi-constants.h \
+ /home/saiko/Documents/stanford/winter_24_25/cs140e/priv-cs140e/libpi/include/your-prototypes.h \
+ pi-sd.h fat32.h mbr.h mbr-helpers.h pi-files.h
+/home/saiko/Documents/stanford/winter_24_25/cs140e/priv-cs140e/libpi/include/rpi.h:
+/home/saiko/Documents/stanford/winter_24_25/cs140e/priv-cs140e/libpi/libc/demand.h:
+/home/saiko/Documents/stanford/winter_24_25/cs140e/priv-cs140e/libpi/include/gpio.h:
+/home/saiko/Documents/stanford/winter_24_25/cs140e/priv-cs140e/libpi/include/rpi-constants.h:
+/home/saiko/Documents/stanford/winter_24_25/cs140e/priv-cs140e/libpi/include/your-prototypes.h:
+pi-sd.h:
+fat32.h:
+mbr.h:
+mbr-helpers.h:
+pi-files.h:
