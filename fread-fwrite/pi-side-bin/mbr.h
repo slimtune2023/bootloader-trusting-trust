c53a051b0ea6a0a4c787f34d5e65a94d443e55c2
Sai Konkimalla <sai.konkimalla@gmail.com>
2025-03-20 15:04:07 -0700

    removing unnecessary stuff

--- fread-fwrite/pi-side-bin/mbr.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RPI_MBR_H__
-#define __RPI_MBR_H__
-#include "pi-sd.h"
-#include "mbr-helpers.h"
-
-// Load the MBR from the disk and verify it.
-mbr_t *mbr_read();
-
-#endif
--- fread-fwrite/test-binaries/README.md
+++
@@ -1,4 +0,0 @@
-Pre-compiled binaries useful for testing your bootloader.
-  - `hello.bin`: print's hello and exits.
-    
-  - `blink-pin20.bin`: does just that.
